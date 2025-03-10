bf861deacc7b522885cb1b71b3ef37d3994e203e
MuthuCapz <muthu@capztone.com>
2025-03-10 07:16:11 +0530

    Mar10(Main page navigate bar, search box)

--- lib/Main/main_page.dart
+++ lib/Main/main_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:eqcart/Profile/profile_page.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainPage());
@@ -14,12 +14,12 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
-  final List<String> _titles = ['Home', 'Cart', 'Favourite', 'Settings'];
+
   final List<Widget> _screens = [
-    Center(child: Text('Home Page', style: TextStyle(fontSize: 20))),
-    Center(child: Text('Cart Page', style: TextStyle(fontSize: 20))),
-    Center(child: Text('Favourite Page', style: TextStyle(fontSize: 20))),
-    Center(child: Text('Settings Page', style: TextStyle(fontSize: 20))),
+    HomeScreen(),
+    CartPage(),
+    FavouritePage(),
+    SettingsPage(),
   ];
 
   void _onItemTapped(int index) {
@@ -33,26 +33,6 @@ class _MainPageState extends State<MainPage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            _titles[_selectedIndex],
-            style: TextStyle(color: Colors.black),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          elevation: 1,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.menu, color: Colors.black),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProfilePage()),
-                );
-              },
-            ),
-          ],
-        ),
         body: _screens[_selectedIndex],
         bottomNavigationBar: Container(
           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
@@ -76,43 +56,202 @@ class _MainPageState extends State<MainPage> {
               bool isSelected = _selectedIndex == index;
               return GestureDetector(
                 onTap: () => _onItemTapped(index),
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 300),
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: isSelected ? Colors.blue : Colors.transparent,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(
-                        [
-                          Icons.home_outlined,
-                          Icons.shopping_cart_outlined,
-                          Icons.favorite_border,
-                          Icons.settings_outlined
-                        ][index],
-                        color: isSelected ? Colors.white : Colors.grey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                      decoration: BoxDecoration(
+                        color: isSelected ? Colors.orange : Colors.transparent,
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      if (isSelected)
-                        Padding(
-                          padding: EdgeInsets.only(left: 8),
-                          child: Text(
-                            _titles[index],
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            [
+                              Icons.home_outlined,
+                              Icons.shopping_cart_outlined,
+                              Icons.favorite_border,
+                              Icons.settings_outlined
+                            ][index],
+                            color: isSelected ? Colors.white : Colors.grey,
                           ),
-                        ),
-                    ],
-                  ),
+                          if (isSelected)
+                            Padding(
+                              padding: EdgeInsets.only(left: 8),
+                              child: Text(
+                                [
+                                  "Home",
+                                  "Cart",
+                                  "Favourite",
+                                  "Settings"
+                                ][index],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               );
             }),
           ),
         ),
       ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(130),
+        child: Container(
+          padding: EdgeInsets.only(top: 40, left: 16, right: 16, bottom: 10),
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min, // Only take needed space
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Deliver to",
+                          style:
+                              TextStyle(fontSize: 14, color: Colors.white70)),
+                      Row(
+                        children: [
+                          Text("Hassan Bin Thabit Street",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white)),
+                          Icon(Icons.keyboard_arrow_down, color: Colors.white),
+                        ],
+                      ),
+                    ],
+                  ),
+                  // Three-dot menu moved OUTSIDE search box
+                  PopupMenuButton<String>(
+                    onSelected: (value) {
+                      if (value == 'Profile') {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProfilePage()),
+                        );
+                      }
+                    },
+                    icon: Icon(Icons.more_vert, color: Colors.white),
+                    itemBuilder: (BuildContext context) => [
+                      PopupMenuItem(
+                        value: 'Profile',
+                        child: Text('Profile'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              // Search Box (now independent)
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search food, groceries & more",
+                    prefixIcon: Icon(Icons.search, color: Colors.grey),
+                    border: InputBorder.none,
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16, vertical: 15),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        // Prevents overflow by making content scrollable
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Welcome to Home Page!",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(height: 20),
+              Container(
+                height: 200,
+                color: Colors.blue[100],
+                child: Center(child: Text("Featured Items")),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 300,
+                color: Colors.green[100],
+                child: Center(child: Text("Popular Items")),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 400,
+                color: Colors.red[100],
+                child: Center(child: Text("More Categories")),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Cart")),
+      body: Center(child: Text("Cart Page", style: TextStyle(fontSize: 20))),
+    );
+  }
+}
+
+class FavouritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Favourite")),
+      body:
+          Center(child: Text("Favourite Page", style: TextStyle(fontSize: 20))),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Settings")),
+      body:
+          Center(child: Text("Settings Page", style: TextStyle(fontSize: 20))),
     );
   }
 }
